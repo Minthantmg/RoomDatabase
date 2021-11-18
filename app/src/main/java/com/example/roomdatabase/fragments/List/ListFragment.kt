e32bf8c1ad1e20f14a2e807f2dbfceb1856d9b99
lenovo <minthant180@gmail.com>
2021-11-18 16:54:24 +0630

    added viewmodel lifecycle for data

--- app/src/main/java/com/example/roomdatabase/fragments/List/ListFragment.kt
+++ app/src/main/java/com/example/roomdatabase/fragments/List/ListFragment.kt
@@ -5,12 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.roomdatabase.R
+import com.example.roomdatabase.data.userViewModel
 import kotlinx.android.synthetic.main.fragment_list.view.*
 
 
 class ListFragment : Fragment() {
+
+    private lateinit var mUserViewModel : userViewModel
 
 
     override fun onCreateView(
@@ -18,6 +25,16 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        val recyclerView = view.recyclerView
+        val adapter = listAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        mUserViewModel = ViewModelProvider(this).get(userViewModel::class.java)
+        mUserViewModel.readAllData.observe(viewLifecycleOwner, Observer { user ->
+            adapter.setData(user)
+        })
 
         view.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
